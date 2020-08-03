a1e752a26cdaa6c59c7a5487cc845a5923d9e35a
Tyler Flowers <ggtylerr_contact@protonmail.com>
2020-08-03 06:27:49 +0000

    Fix script loading in HTTPS.

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/v', (req,res) => {
         <link rel="stylesheet" href="res/styles/dark.css">
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="http://d3g0gp89917ko0.cloudfront.net/v--3e3a6f7dbcc9/common--javascript/init.combined.js"></script>
+        <script type="text/javascript" src="https://d3g0gp89917ko0.cloudfront.net/v--3e3a6f7dbcc9/common--javascript/init.combined.js"></script>
       </head>
       <body>
         <div id="main-content">
